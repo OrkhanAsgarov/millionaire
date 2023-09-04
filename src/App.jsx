9810bcb0587601cd13a1dd19751fe695a2c12c6e
OrkhanAsgarov <asgarov.orkhan89@gmail.com>
2023-09-04 13:01:51 +0300

    response design done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { GiDiamonds } from "react-icons/gi";
 
 function App() {
   const [userName, setUserName] = useState(null);
-  const [questionNumber, setQuestionNumber] = useState(4);
+  const [questionNumber, setQuestionNumber] = useState(1);
   const [stop, setStop] = useState(false);
   const [chooseVariant, setChooseVariant] = useState(false);
   const [earned, setEarned] = useState("$ 0");
